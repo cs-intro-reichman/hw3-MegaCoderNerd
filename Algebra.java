a4f557470cfea3f0d35c2978468e4241dfbb4eba
Omer <godmega67@gmail.com>
2024-11-19 11:54:12 +0200

    bugs with anagram need fixing

--- Algebra.java
+++ Algebra.java
@@ -25,43 +25,109 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		// If x2 is negative we just call minus instead
+		if (x2 < 0) return minus(x1, -x2);
+		// to not overwrite x1
+		int sum = x1;
+		// incrementing sum, x2 times to get the sum
+		for (int i = 0; i<x2; i++) sum++;
+		return sum;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		// If x2 is negative we just call plus instead
+		if (x2 < 0) return plus(x1, -x2);
+		// to not overwrite x1
+		int diff = x1;
+		// decrementing diff, x2 times to get the difference
+		for (int i = 0; i < x2; i++) diff--;
+		return diff;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
+		int product = 0;
+        // we call plus, x2 times to do this operation
+		for (int i = 0; i < Math.abs(x2); i++) product = plus(product, x1);
+
+		// If x2 is negative we will just inverse the result
+		if (x2 < 0) product = minus(0, product);
+		return product;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		// if n == 0 then the result will be 1 no matter what
+		int result = 1;
+		// we will call times function n times
+		for (int i = 0; i < n; i++) result = times(result, x);
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
+		// as we are talking about integers we will just return 0 if x2 is 0
+		if (x2 == 0) return 0;
+		// so no overwriting will be done to x1
+        int temp = Math.abs(x1);
+		int product = 0;
+		/*  we will call minus function with x2 until negative (which is the remainder)
+			to get the product itself we will count the amount of iterations it took
+		*/
+		while (temp >= x2){
+			temp = minus(temp, Math.abs(x2));
+			product++;
+		}
+		/*  as we want the division of negatives if both are negatives
+			they will cancel out otherwise,
+			we must return the inverse of the product */
+
+		if (x2 < 0 || x1 < 0) product = minus(0, product);
+
+		return product;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-        // Replace the following statement with your code
-		return 0;
+        /*  we do division and then we simply multiply x2 by the product
+			then we get the difference of x1 and the result,
+			which gives us the remainder 
+		*/
+	
+		// mod(0) is undefined as it's division by 0, thus we return -1
+		if (x2 == 0) return -1;
+        return minus(x1,times(div(x1, x2), x2));
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-        // Replace the following statement with your code
-		return 0;
-	}	  	  
+		// we will use newton raphson as it's the most elegant
+
+		// no negative roots
+		if (x < 0) {
+            return -1;
+        }
+
+        if (x == 0 || x == 1) {
+            return x; // sqrt(0) = 0, sqrt(1) = 1
+        }
+
+        // Initial guess
+	    // start from the middle as we already checked edge cases
+        int guess = div(x,2); 
+
+        // Newton-Raphson iteration
+		// Formula: 
+		// g - (g^2-x)/(2*g) = g - (g^2/g - x/g)/2
+		// = g- (g/2-x/2g) = g/2 + x/2g = (g+x/g)/2
+		// the last part is the one I use
+        while (true) {
+            int next = div(plus(guess, div(x, guess)), 2);
+            if (next >= guess) break;
+            guess = next; // Update the guess
+        }
+
+        return guess;
+    }
 }
--- Anagram.java
+++ Anagram.java
@@ -18,7 +18,7 @@ public class Anagram {
 		String str = "1234567";
 		Boolean pass = true;
 		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 1000; i++) {
 			String randomAnagram = randomAnagram(str);
 			System.out.println(randomAnagram);
 			pass = pass && isAnagram(str, randomAnagram);
@@ -29,22 +29,53 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		if (str1.length() != str2.length()) return false;
+		for (int i = 0; i < str1.length(); i++){
+			char curr = str1.charAt(i);
+			if (str2.indexOf(curr, i) == -1) return false;
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	private static String preProcess(String str) {
-		// Replace the following statement with your code
-		return str;
+		// turning str to undercase letters
+		str = str.toLowerCase();
+		// making new variables one which we return and is the edited string
+		// and one which is all the characters we want to remain (spaces and alphabet)
+		String acceptedChars = "abcdefghijklmnopqrstuvwxyz ";
+		String editedStr = "";
+		// going over all the characters and only add those which are accepted
+		// to the processed string
+		for (int i=0; i < str.length(); i++){
+			char curr = str.charAt(i);
+			if (acceptedChars.indexOf(curr) != -1){
+				editedStr += curr;
+			}
+		}
+		return editedStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return str;
+		// no other anagram if the string is empty or only one character
+		if (str.length() <= 1) return str; 
+		str = preProcess(str);
+		String newStr = "";
+		itn len = str.length()
+		
+		// we go over all of str and make random indexes which
+		// are unique to access characters from
+		for (int i = 0; i < len; i++){
+			int j = (int) Math.min((Math.random()*str.length() + i), str.length()-i-1);
+			newStr += str.charAt(j+i);
+			//str.replace(str.charAt(j+i),"")
+		}
+		return newStr;
 	}
 }
